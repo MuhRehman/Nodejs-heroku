f57de6b11e1a2906106387773bb349db11bd06a3
MuhRehman <paki_jan@yahoo.com>
2020-10-26 12:14:32 -0700

    rehman

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://rehman:rehman123@cluster0.s9oi5.mongodb.net/testing?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
